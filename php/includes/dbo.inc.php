7c742f2ecc9ef39b26aac88835eaf1cd90088376
DESKTOP-8KTM3PB\frede <frederic.vanovermeire@gmail.com>
2023-06-03 11:57:53 +0200

    set .env

--- php/includes/dbo.inc.php
+++ php/includes/dbo.inc.php
@@ -1,8 +1,9 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "chalets";
+$dbconfig = parse_ini_file(".env");
+$db_host = $dbconfig["DB_HOST"];
+$db_user = $dbconfig["DB_USER"];
+$db_pass = $dbconfig["DB_PW"];
+$db_name = $dbconfig["DB_NAME"];
 
 $link = new mysqli($db_host, $db_user, $db_pass, $db_name);
 $charset = "utf8";
